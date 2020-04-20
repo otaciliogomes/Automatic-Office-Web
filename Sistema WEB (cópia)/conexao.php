3f4d541b6f1034c8a8b79d0ec5b633f4db8c37f8
Otacilio <otaciliogsantos@gmail.com>
2020-04-20 20:37:07 -0300

    back

--- Sistema WEB (cópia)/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', 'Codigo20');
-define('DB', 'login');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Acesso Invalido');
--- Sistema WEB (cópia)/js/cadastro.js
+++
@@ -1,9 +0,0 @@
-function mostrar(elemento){
-    var display = document.getElementById(elemento).style.display;
-      if(display == "none"){
-          document.getElementById(elemento).style.display = 'block';
-      }else{
-          document.getElementById(elemento).style.display = 'none';
-      }    
-  }
-
